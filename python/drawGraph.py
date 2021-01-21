3800f57767671fae7894003719b236ba767d8b54
梁哈特 <50618076+Belfast1924@users.noreply.github.com>
2021-01-21 15:22:27 +0800

    Version 1.1 of drawGraph.py script
    Some optimization of structure and functions.
    Which would bring more convenience and flexibility.

--- python/drawGraph.py
+++ python/drawGraph.py
@@ -1,30 +1,63 @@
-import matplotlib.pyplot as plt
-import time
-import dataToDB
+#!/usr/bin/env python3
+# -*- coding:"utf-8" -*-
 
-'''
+
+"""
     @Author Harter·Liang
-    @Date 2021/01/20
-    @Version 1.0
-    @Description Plot the line chart of temperature change using matplotlib
-'''
+    @Date 2021/01/21
+    @Version 1.1
+    @Description Draw the graph of the temperature and time from the database.
+    @Upgrades With combined functions, which bring more flexibility.
+"""
 
-# Get the dataset
-res = dataToDB.getData("TEMP_5SEC")
+import matplotlib.pyplot as plt
 
-xList = []
-yList = []
+from dbCon.dataToDB import *
 
-# add data to list
-for content in res:
-    xList.append(content[1])
-    yList.append(content[2])
 
-# since data is ordered in most recent added, reversion is needed
-xList.reverse()
+# Depending the package which includes the script dataToDB.py
 
-# plot and save the image
-plt.xlabel("Time (Second)")
-plt.ylabel("Temperature (Celsius)")
-plt.plot(xList, yList, color='green', linewidth=3)
-plt.savefig("TestFig.png")
+
+def getGraphData(tbName):
+    # Changing the function for the usage of getting data from database
+    graphRawData = getData(tbName)
+    # Get the date from the database with the function of package dbCon
+
+    xList = []
+    yList = []
+    # Save the data
+
+    for content in graphRawData:
+        xList.append(content[1])
+        yList.append(content[2])
+        # Appending the data from the content returned from SQL Query execution
+
+    xList.reverse()
+    # Get the right order of the data
+
+    return xList, yList
+
+
+def drawGraph(contentSource, imgName, timePeriodName):
+    # The aim of this function is simplified for drawing the pictures
+    xList = contentSource[0]
+    yList = contentSource[1]
+    # The data source for x and y label data
+    plt.xlabel(timePeriodName)
+    plt.ylabel("Temperature (Celsius)")
+    # X and Y label of the plot fig
+    plt.plot(xList, yList, color='green', linewidth=3)
+    # Attribute of the pic
+    plt.savefig(str(imgName) + ".png")
+    # Save the picture
+
+
+def drawGraphTest(tbName, imgName, timePeriodName):
+    # Sample of combined functions
+    rawContent = getGraphData(tbName)
+
+    drawGraph(rawContent, imgName, timePeriodName)
+
+
+if __name__ == "__main__":
+    drawGraphTest("TEMP_5SEC", "Sample", "Time (seconds)")
